c1c95042b6d7937f0e2efe392aa7fcefd0a15904
Chase Overcash <covercas@asue.edu>
2019-04-10 20:59:11 -0700

    + Test Cases + Demo.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -223,17 +223,17 @@ Token LexicalAnalyzer::GetToken()
             return tmp;
     }
 }
-
-int main()
-{
-    LexicalAnalyzer lexer;
-    Token token;
-
-    token = lexer.GetToken();
-    token.Print();
-    while (token.token_type != END_OF_FILE)
-    {
-        token = lexer.GetToken();
-        token.Print();
-    }
-}
+//
+//int main()
+//{
+//    LexicalAnalyzer lexer;
+//    Token token;
+//
+//    token = lexer.GetToken();
+//    token.Print();
+//    while (token.token_type != END_OF_FILE)
+//    {
+//        token = lexer.GetToken();
+//        token.Print();
+//    }
+//}
